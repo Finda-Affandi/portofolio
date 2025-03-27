f0daa39fd0e4ecbcc2a7bf3ac530bff3022973cb
Finda Affandi <finda.affandi@sat.co.id>
2025-03-27 10:41:08 +0700

    Adjust background

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -2,21 +2,19 @@ import {COLOR} from "../constants/UIConfig.jsx";
 
 export const Background = () => {
     return (
-        <div className="flixed top-0 -z-10 h-full w-full">
+        <div
+            style={{
+                background: COLOR.background,
+                backgroundImage: `linear-gradient(to right, ${COLOR.idleButton} 1px, transparent 1px),
+                      linear-gradient(to bottom, ${COLOR.idleButton} 1px, transparent 1px)`,
+                backgroundSize: "6rem 4rem"
+            }}
+            className="absolute inset-0 -z-10 h-full w-full">
             <div
                 style={{
-                    background: COLOR.background,
-                    backgroundImage: `linear-gradient(to right, ${COLOR.idleButton} 1px, transparent 1px),
-                      linear-gradient(to bottom, ${COLOR.idleButton} 1px, transparent 1px)`,
-                    backgroundSize: "6rem 4rem"
+                    backgroundImage: `radial-gradient(circle 800px at 100% 200px, ${COLOR.background} 1%, transparent)`,
                 }}
-                className="absolute inset-0 -z-10 h-full w-full">
-                <div
-                    style={{
-                        backgroundImage: `radial-gradient(circle 800px at 100% 200px, ${COLOR.secondary} 1%, transparent)`,
-                    }}
-                    className="absolute bottom-0 left-0 right-0 top-0">
-                </div>
+                className="absolute bottom-0 left-0 right-0 top-0">
             </div>
         </div>
     )
