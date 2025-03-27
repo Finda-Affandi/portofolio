a284d6cc8bb92f4cf49ed19fbf627507a9b13ec5
Finda Affandi <finda.affandi@sat.co.id>
2025-03-27 10:42:21 +0700

    adjust container, navbar, and add variable proximity

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,15 @@
 import logo from "../assets/io.png"
+import {Button} from "./Button.jsx";
 
 export const Navbar = () => {
     return (
         <nav className="mb-20 flex items-center justify-between py-6">
             <div className="flex flex-shrink-0 items-center">
-                <img src={logo} alt="Logo" className="w-[1.5%] h-auto" />
+                <img src={logo} alt="Logo" className="w-20 h-auto" />
             </div>
-            <div className="m-8 flex items-center justify-center gap-4 text-2xl"></div>
+            <div className="m-8 flex items-center justify-center gap-4">
+                <Button text="Reach Out"/>
+            </div>
         </nav>
     )
 }
